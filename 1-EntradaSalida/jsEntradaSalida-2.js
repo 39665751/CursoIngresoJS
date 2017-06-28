ffbf1dcca81d3dbe438c1be2113325dd3fe85b49
Merino Cristian <cristian.merino.123@hotmail.com>
2017-06-28 20:32:16 -0300

    Primer Commit
    Subimos los 2 primeros ejercicios.

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,9 @@
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
-	
+	var nombre = prompt("Por favor, ingresa tu nombre", "Natalia Natalia");
+	//var :designa una variable. es una porcion de memoria ram
+	//prompt():Ingreso de datos del usuario.
+	alert("Su nombre es "+nombre);
 }
 
