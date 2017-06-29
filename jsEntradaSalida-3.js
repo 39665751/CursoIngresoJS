85fcd7d99d2989977e25751cea7a0921109b12bb
Merino Cristian <cristian.merino.123@hotmail.com>
2017-06-28 22:06:57 -0300

    commit 2
    avanzamos con document.getElementById()

---
+++ jsEntradaSalida-3.js
@@ -0,0 +1,14 @@
+/*Debemos lograr tomar un dato por 'ID'
+y luego mostrarlo por 'Alert' al presionar el botón  'MOSTRAR'*/
+function Mostar()
+{
+	var dato=0;
+	dato="octavio" //al sobreescribir la variable, se borra lo anterior
+	//alert(dato);
+	/*var dato=document.getElementById('elNombre').value;
+	alert(dato);*/
+	document.getElementById('elNombre').value="Natalia Natalia"
+}
+
+//Google chrome. f12. 
+//si hay error, en la consola aparecen los errores.
---
+++ jsEntradaSalida-4.js
@@ -0,0 +1,10 @@
+/* 
+	Debemos lograr tomar un dato por 'PROMPT' 
+	y lo muestro por 'getElementById' al presionar el botón 'MOSTRAR'
+*/
+function Mostar()
+{
+	var dato=prompt("ingresa tu nombre");
+	document.getElementById("elNombre").value=dato;
+}
+
