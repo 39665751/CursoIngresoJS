5d9b2c1ad7995b0346ee21f647d600bd36317711
Merino Cristian <cristian.merino.123@hotmail.com>
2017-07-07 22:13:03 -0300

    Iniciamos y comparamos SWITCH con IF.

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -1,9 +1,50 @@
 function Mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
-
-
-
+	var mesDelAño = document.getElementById('mes').value;
+	/*if (mesDelAño=='Enero')
+	{
+		alert("que comiences bien el año!!!.");
+	}
+	else 
+	if(mesDelAño=='Marzo')
+	{
+		alert("a clases!!!.");
+	}
+	else 
+	if (mesDelAño=='Julio')
+	{
+		alert("se vienen las vacaciones!!!.");
+	}
+	else
+	if(mesDelAño=='Diciembre')  
+	{
+		alert("Felices fiestas!!!.");
+	}
+	else{
+			alert("en este mes no pasa nada.")
+		}*/
+		switch (mesDelAño) {
+    case 'Enero':
+        alert("que comiences bien el año!");
+        break;
+    case 'Marzo':
+        alert("Comienzan las calses!");
+        break;
+    case 'Julio':
+        alert("Se vienen las vacaciones!");
+        break;
+    case 'Diciembre':
+        alert("Felices fiestas!");
+        break;
+    case 4:
+        day = "Thursday";
+        break;
+    case 5:
+        day = "Friday";
+        break;
+    case 6:
+        day = "Saturday";
+}
 
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -5,7 +5,35 @@ var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
 	
+	switch (mesDelAño){
+	case'Enero':
+		alert("Este mes tiene mas de 30 Dias!");
+		break;
+	case'Febrero':
+		alert("Este mes no tiene mas de 29 Dias!");
+		break;
+	case'Marzo':
 	
+	case'Abril':
+	
+	case'Mayo':
+	
+	case'Junio':
+	
+	case'Julio':
+	
+	case'Agosto':
+	
+	case'Septiembre':
+	 
+	case'Octubre':
+	 
+	case'Noviembre':
+	 
+	case'Diciembre':
+	 alert("Este mes tiene mas de 30 Dias!");
+	break;
+	}
 
 
 }//FIN DE LA FUNCIÓN
